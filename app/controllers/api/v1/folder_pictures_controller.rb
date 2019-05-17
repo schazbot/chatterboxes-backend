e16158446e1b9b5d4f28820c16f656f3f9fee854
schazbot <charlotte.pearce1984@gmail.com>
2019-05-17 12:02:12 +0100

    added create functions to controllers

--- app/controllers/api/v1/folder_pictures_controller.rb
+++ app/controllers/api/v1/folder_pictures_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::FolderPicturesController < ApplicationController
 
+    def create
+        folder_picture = FolderPicture.create(folder_id: params[:folder_id], picture_id: params[:picture_id])
+        render json: folder_picture
+    end
+
+
 end
