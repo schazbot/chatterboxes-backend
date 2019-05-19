a61661e49f8d43931459bda591b22b8ee7bb6568
schazbot <charlotte.pearce1984@gmail.com>
2019-05-19 12:59:15 +0100

    slight change to creat controller - now works

--- app/controllers/api/v1/folder_pictures_controller.rb
+++ app/controllers/api/v1/folder_pictures_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::FolderPicturesController < ApplicationController
     end
 
     def create
-        folder_picture = FolderPicture.create(folder_id: params[:folder_id], picture_id: params[:picture_id])
+        pic_id =Picture.last.id
+        folder_picture = FolderPicture.create(folder_id: params[:folder_id], picture_id: pic_id)
         render json: folder_picture
     end
 
