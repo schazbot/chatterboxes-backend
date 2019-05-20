9183a96d0386cd6094ff56bae2ff38d200a79a9d
schazbot <charlotte.pearce1984@gmail.com>
2019-05-20 17:15:19 +0100

    strong params and update control methods added to pictures. Edits being saved to backend

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::FoldersController < ApplicationController
 
+    def index
+        folders = Folder.all 
+        render json: folders
+    end
+
     def show
         folders = Folder.find(params[:id])
         render json: folders
@@ -9,5 +14,20 @@ class Api::V1::FoldersController < ApplicationController
         folder = Folder.create(name: params[:name],  user_id:"1", image_url: params[:image_url])
         render json: folder
     end
+
+    def update
+        folder = Folder.find(params[:id])
+        folder.update(folder_params)
+        render json: folder
+    end
+
+    
+    
+      private
+    
+      def folder_params
+          params.require(:folder).permit(:name)
+      end
+
 
 end
