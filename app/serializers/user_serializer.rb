6dd388dfa86077e42df6d38ab3112ba5e9514089
schazbot <charlotte.pearce1984@gmail.com>
2019-05-24 14:39:23 +0100

    user serialiser uncommented

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,22 +4,22 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id
   has_many :folders
 
-  # def folders
-  #   object.folders.map do |folder|
-  #     {
-  #       id: folder.id,
-  #       name: folder.name,
-  #       image_url: folder.image_url,
-  #       pictures: folder.pictures.map do |p|
-  #         {
-  #           id: p.id,
-  #           text: p.text,
-  #           url: p.url
-  #         }
-  #       end
-  #     }
-  #   end 
+  def folders
+    object.folders.map do |folder|
+      {
+        id: folder.id,
+        name: folder.name,
+        image_url: folder.image_url,
+        pictures: folder.pictures.map do |p|
+          {
+            id: p.id,
+            text: p.text,
+            url: p.url
+          }
+        end
+      }
+    end 
 
-  # end
+  end
 
 end
